151207154048eab6eec951fe937fa70ad3bd7f1f
ArtNiki <TEMA_9@HOTMAIL.COM>
2021-07-07 20:27:09 -0400

    Commit2

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello bubu");
+        System.out.println("I don't know why but the good thing we can re watch it ");
 
     }
 }
