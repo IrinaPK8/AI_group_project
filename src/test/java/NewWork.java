d7ba374a591050fe73f9a2e3ce68e6827d69ee00
IrinaPK8 <irina.pk8@gmail.com>
2021-07-07 21:10:13 -0400

    finished new part

--- src/test/java/NewWork.java
+++ src/test/java/NewWork.java
@@ -2,5 +2,6 @@ public class NewWork {
     public static void main(String[] args) {
         System.out.println("New work");
         System.out.println("Working on new part");
+        System.out.println("Finished new part");
     }
 }
